aaf3d74f74a1573d17042e4352a524a738201a3e
dtbaker <dtbaker@gmail.com>
2016-09-13 13:51:41 +1000

    statement csv format change

--- plugins/statementer.js
+++ plugins/statementer.js
@@ -755,13 +755,13 @@ tabcount++;
                             name: line[3],
                             id: parseInt(line[4], 10) || null,
                             document: parseInt(line[5], 10) || null,
-                            earnings: parseFloat(line[10]),
+                            earnings: parseFloat(line[11]),
                             rate: null,
                             price: parseFloat(line[6]),
                             us_rwt: parseFloat(line[8]), // from 0 to 30%
                             us_bwt: parseFloat(line[9]), // if haven't submited W-8 form.
-                            site: line[11].replace('"', ''),
-                            country: line[12],
+                            site: line[12].replace('"', ''),
+                            country: line[13],
                             purchased: false,
                             order_id: order_id
                         };
@@ -1043,7 +1043,7 @@ tabcount++;
 								$content.html('<span style="font-style:italic">fetching data...</span>');
 								$.get(csvfile, function (data) {
 									//raw = $.trim(data.replace(/"Date","Order ID","Type","Detail","Item ID","Price","Amount"\n/g, ''));
-									raw = $.trim(data.replace(/"Date","Order ID","Type","Detail","Item ID","Document","Price","EU VAT","US RWT","US BWT","Amount","Site","Other Party Country","Other Party Region","Other Party City","Other Party Zipcode"\n/g, ''));
+									raw = $.trim(data.replace(/"Date","Order ID","Type","Detail","Item ID","Document","Price","EU VAT","US Sales Tax","US RWT","US BWT","Amount","Site","Other Party Country","Other Party Region","Other Party City","Other Party Zipcode"\n/g, ''));
 									save('' + currentyear + '_' + currentmonth, raw);
 									window.dashboardplus.setCookie('statementer_lastbalance', currentbalance, 30);
 									initCalc();
@@ -1056,7 +1056,7 @@ tabcount++;
 							clear();
 							$content.html('<span style="font-style:italic">fetching data...</span>');
 							$.get(csvfile, function (data) {
-								raw = $.trim(data.replace(/"Date","Order ID","Type","Detail","Item ID","Document","Price","EU VAT","US RWT","US BWT","Amount","Site","Other Party Country","Other Party Region","Other Party City","Other Party Zipcode"\n/g, ''));
+								raw = $.trim(data.replace(/"Date","Order ID","Type","Detail","Item ID","Document","Price","EU VAT","US Sales Tax","US RWT","US BWT","Amount","Site","Other Party Country","Other Party Region","Other Party City","Other Party Zipcode"\n/g, ''));
 								initCalc();
 							});
 						}
